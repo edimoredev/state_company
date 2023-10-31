abea20a50936033d229b17f86af32f4dc17de555
edmoreno8704 <edisonmc@digitalwareadvisory.com>
2023-10-31 16:37:56 -0500

    [FEATURE] create model property

---
+++ backend/app/models/property_model.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class Property(BaseModel):
+    id_property: int
+    name: str
+    address:str
+    price:float
+    code_internal:str
+    year:int
+    id_owner:int
