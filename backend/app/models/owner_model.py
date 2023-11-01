836f969d017303ef7daacce8dae9f561a9927597
edmoreno8704 <edisonmc@digitalwareadvisory.com>
2023-11-01 15:00:53 -0500

    [FEATURE] create models owner_model

--- backend/app/models/owner_model.py
+++ backend/app/models/owner_model.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel
 
-class Owner(BaseModel):
+class OwnerModel(BaseModel):
+    """
+    Class representing an owner.
+    """
     id_owner: int
     name: str
-    address:str
-    photo:str
-    birhday: str
+    address: str
+    photo: str
+    birthday: str
