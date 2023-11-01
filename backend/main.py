a948fdb9e45099b94f0f213c31ee34f18a46a238
edmoreno8704 <edisonmc@digitalwareadvisory.com>
2023-11-01 14:50:48 -0500

    [Feature] add include reuter ownerRoute

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import property_route
+from app.routers import property_route, owner_route
 
+# Create an instance of the FastAPI application
 app = FastAPI()
 
+# Define settings to allow requests from any origin
 origins = ["*"]
 
+# Add middleware to enable Cross-Origin Resource Sharing (CORS)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,      # Allow requests from origins specified in 'origins'
+    allow_credentials=True,     # Allow including credentials in requests (e.g., cookies)
+    allow_methods=["*"],        # Allow any HTTP method (GET, POST, etc.)
+    allow_headers=["*"],        # Allow any headers in requests
 )
 
-app.include_router(property_route.propertyRouter)
-# app.include_router(typeTransactions.typeTransactionRouter)
-# app.include_router(accounts.accountRouter)
-# app.include_router(transactions.transactionRouter)
+# includes the defined routers
+app.include_router(owner_route.ownerRouter)   # Include routes related to owners
+app.include_router(property_route.propertyRouter)  # Include routes related to properties
+
+
