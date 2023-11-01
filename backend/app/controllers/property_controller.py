4d7ebf09aa21ba5505890fb93006dbfde9fe4983
edmoreno8704 <edisonmc@digitalwareadvisory.com>
2023-11-01 14:55:34 -0500

    [FEATURE] change variable property

--- backend/app/controllers/property_controller.py
+++ backend/app/controllers/property_controller.py
@@ -4,8 +4,8 @@ from app.adapters.database.dbmongo import conn
 class PropertyController():
 
     def get_all_property(self):
-        users = conn.property.find()
-        return list(users)
+        property = conn.property.find()
+        return list(property)
     
     def create_property(self, property):
         conn.property.insert_one(property)
