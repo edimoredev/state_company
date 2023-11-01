10c8484c59ca1c4b58349878f66944bcc7332c63
edmoreno8704 <edisonmc@digitalwareadvisory.com>
2023-11-01 16:40:13 -0500

    [Style] change comments

--- backend/app/routers/owner_route.py
+++ backend/app/routers/owner_route.py
@@ -15,15 +15,19 @@ ownerRouter = APIRouter(
 # Define a route to create an owner
 @ownerRouter.post('/', response_model=OwnerModel, status_code=status.HTTP_201_CREATED)
 async def create_owner(owner: OwnerModel):
+    """
+    Create a new owner in the database.
+    :param owner: Data of the owner to create.
+    """
     # Check if the owner already exists
-    existing_owner = owners_schema(OwnerController().search_owner_by_id_owner(owner.id_owner))
-     # Valid existing_owner
+    existing_owner = owners_schema(OwnerController().search_owner_by_id(owner.id_owner))
+    # Valid existing_owner
     if existing_owner:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Owner already exists"
         )
-    # Transform to dic
+    # Convert to a dictionary
     owner_dict = dict(owner)
     # Create the owner using the controller
     OwnerController().create_owner(owner_dict)
@@ -32,6 +36,9 @@ async def create_owner(owner: OwnerModel):
 # Define a route to get all owners
 @ownerRouter.get('/', response_model=list[OwnerModel])
 async def get_all_owner():
+    """
+    Get a list of all owners in the database.
+    """
     # Get all owners using the controller
     owners = owners_schema(OwnerController().get_all_owner())
     # Valid owners and retur message
@@ -43,10 +50,15 @@ async def get_all_owner():
     return owners
 
 # Define a route to get an owner by their ID
-@ownerRouter.get('/{owner}')
-async def get_owner(owner: int):
+@ownerRouter.get('/{id_owner}')
+async def get_owner(id_owner: int):
+    """
+    Get an owner by their ID.
+
+    :param id_owner: ID of the owner to search for.
+    """
     # Search for an owner by their ID using the controller
-    owner = owners_schema(OwnerController().search_owner_by_id_owner(owner))
+    owner = owners_schema(OwnerController().search_owner_by_id(id_owner))
     # Valid owner and retur message
     if not owner:
         raise HTTPException(
