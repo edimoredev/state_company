61ce3743de8a2020e179eb5983eacfbe6c9dfc30
edmoreno8704 <edisonmc@digitalwareadvisory.com>
2023-11-02 13:01:27 -0500

    [FEATURE] create controller propertyImage_controller

---
+++ backend/app/controllers/propertyImage_controller.py
@@ -0,0 +1,44 @@
+from fastapi import HTTPException, status
+from app.adapters.database.dbmongo import conn
+import pymongo
+
+
+class PropertyImageController():
+    """
+    Class that manages operations related to property in the database.
+    """
+    def create_propertyImage(self, id_property_image):
+        """
+        Creates a new propertyImage in the database.
+        :param id_property_image: A dictionary representing the propertyImage to be created.
+        """
+        try:
+            conn.property_image.insert_one(id_property_image)
+        except:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Database connection not found")
+
+    def get_all_propertyImage(self):
+        """
+        Retrieves a list of all property from the database.
+        :return: A list of property dictionaries.
+        """
+        try:
+            property = conn.property_image.find().sort("id_property_image", pymongo.ASCENDING)
+            return list(property)
+        except:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Database connection not found")
+        
+    def search_propertyImage_by_id(self, id_property_image: int) -> dict:
+        """
+        Searches for an propertyImage in the database by their ID.
+        :param id_property_image: The ID of the propertyImage to search for.
+        :return: The propertyImage dictionary if found, or None.
+        """
+        try:
+            property = conn.property_image.find({'id_property_image': id_property_image})
+            return property
+        except:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Database connection not found")
