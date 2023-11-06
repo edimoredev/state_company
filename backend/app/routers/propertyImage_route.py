53181fd1285e2747ede97d2a18c76ff8893ec0ba
edmoreno8704 <edisonmc@digitalwareadvisory.com>
2023-11-06 17:49:50 -0500

    [REFACTOR] change imports property_route and validates

--- backend/app/routers/propertyImage_route.py
+++ backend/app/routers/propertyImage_route.py
@@ -1,37 +1,59 @@
-from fastapi import APIRouter, HTTPException, status
-from app.schemas.propertyImage_schema import propertiesImage_schema
+from fastapi import APIRouter, HTTPException, status, UploadFile, File, Depends
+import os
+from schemas.propertyImage_schema import propertiesImage_schema
+from schemas.property_schema import properties_schema
 #>>> Import controllers
-from app.controllers.propertyImage_controller import PropertyImageController
+from controllers.propertyImage_controller import PropertyImageController
+from controllers.property_controller import PropertyController
 #>>> Import models
-from app.models.propertyImage_model import PropertyImageModel
+from models.propertyImage_model import propertyImageOut, PropertyImageModel
+
+
+PATH_FILES =  os.getcwd() + "\\app\\asset\\img\\"
 
 # Create a router to handle operations related to propertyImage
 propertyImageRouter = APIRouter(prefix="/propertyImage",
                        tags=["PropertyImage"],
                        responses={status.HTTP_404_NOT_FOUND: {"message": "Not Found"}})
 
-@propertyImageRouter.post('/',response_model= PropertyImageModel, status_code = status.HTTP_201_CREATED)
-async def create_propertyImage(propertyImage: PropertyImageModel):
+@propertyImageRouter.post('/', response_model= propertyImageOut,  status_code = status.HTTP_201_CREATED)
+async def create_propertyImage(propertyImage:PropertyImageModel = Depends(), image: UploadFile = File(...)):
     """
     Create a new propertyImage in the database.
     :param propertyImage: Data of the propertyImage to create.
     """
-    # Check if the propertyImage already exists
+    # Validar la extensión del archivo
+    if not PropertyImageController().allowed_file(image.filename):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Solo se permiten archivos .png, .jpg, .jpeg")
+    
+    #  Check if the propertyImage already exists
     existing_propertyImage = propertiesImage_schema(PropertyImageController().search_propertyImage_by_id(propertyImage.id_property_image))
-    # Valid existing_propertyImage
+    #   Valid existing_propertyImage
     if existing_propertyImage:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="PropertyImage already exists"
         )
+
+    # Check if the property not already exists
+    existing_property = properties_schema(PropertyController().search_property_by_id(propertyImage.id_property))
+    if not existing_property:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Property does not exists"
+        )
     # Convert to a dictionary
     propertyImage_dict = dict(propertyImage)
+    propertyImage_dict['file_name'] = image.filename
+    print (propertyImage_dict)
     # Create the propertyImage using the controller
     PropertyImageController().create_propertyImage(propertyImage_dict)
-    return propertyImage
+    with open(PATH_FILES + image.filename, "wb") as img:
+        img.write(image.file.read())
+    return  propertyImage_dict
 
 # Define a route to get all propertiesImage
-@propertyImageRouter.get('/',response_model= list[PropertyImageModel])
+@propertyImageRouter.get('/',response_model= list[propertyImageOut])
 async def get_all_propertyImage():
     """
     Get a list of all propertiesImage in the database.
