fbcddb6f0c4e4edc9fabf202945f55f017f33d0f
edmoreno8704 <edisonmc@digitalwareadvisory.com>
2023-11-06 17:44:24 -0500

    [REFACTOR] change typing and class propertyImageOut

--- backend/app/models/propertyImage_model.py
+++ backend/app/models/propertyImage_model.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class PropertyImageModel(BaseModel):
     """
     Class representing an propertyImageModel.
     """
     id_property_image: int
-    file: str
     enable: bool
     id_property: int
+
+class propertyImageOut(PropertyImageModel):
+    file_name: Optional[str]
